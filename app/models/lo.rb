bae963aefb12f4fbea4e2eb96d7f49c18fcd64f3
isabela-tvitek <isat12vitek@gmail.com>
2023-08-15 22:20:04 -0300

    feat(api): correction in the crud

--- app/models/lo.rb
+++ app/models/lo.rb
@@ -1,11 +1,3 @@
 class Lo < ApplicationRecord
-  include ActiveModel::Model
-
-  attr_accessor :title, :description, :image
-
   validates :title, :description, :image, presence: true
-
-  def id
-    nil
-  end
 end
