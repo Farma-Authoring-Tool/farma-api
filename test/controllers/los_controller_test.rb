f396889d52e54c6cc91cdf2be0bc7bbc7a838185
isabela-tvitek <isat12vitek@gmail.com>
2023-08-21 22:12:36 -0300

    feat(api): added the tests and made the correction of the controller

--- test/controllers/los_controller_test.rb
+++ test/controllers/los_controller_test.rb
@@ -1,6 +1,35 @@
 require 'test_helper'
 class LosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get index' do
+    get los_url
+
+    assert_response :success
+  end
+
+  test 'should show lo' do
+    lo = los(:one)
+    get los_url(lo)
+
+    assert_response :success
+  end
+
+  test 'should create lo' do
+    post los_url, params: { lo: { title: 'title lo', description: 'description', image: 'image name' } }, as: :json
+
+    assert_response :success
+  end
+
+  test 'should update lo' do
+    lo = los(:one)
+    patch lo_url(lo), params: { lo: { title: 'updated title' } }, as: :json
+
+    assert_response :success
+  end
+
+  test 'should destroy lo' do
+    lo = los(:one)
+    delete lo_url(lo)
+
+    assert_response :success
+  end
 end
--- test/models/lo_test.rb
+++ test/models/lo_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 class LoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'the truth' do
+    lo = Lo.new
+    lo.save
+  end
 end
