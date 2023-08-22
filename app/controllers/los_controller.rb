e42eb345273c53dd72720c88dbf22f8a698f1aab
isabela-tvitek <isat12vitek@gmail.com>
2023-08-21 22:12:36 -0300

    feat(api): added the tests and made the correction of the controller

--- app/controllers/los_controller.rb
+++ app/controllers/los_controller.rb
@@ -12,40 +12,22 @@ class LosController < ApplicationController
     if lo.save
       render json: lo, status: :created
     else
-      render json: { lo: lo, errors: lo.errors}, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    lo = Lo.new(lo_params)
-    if lo.update(lo_params)
-      render json: lo, status: :update
-    else
-      render json: { lo: lo, errors: lo.errors}, status: :unprocessable_entity
+      render json: { lo: lo, errors: lo.errors }, status: :unprocessable_entity
     end
   end
 
   def update
     lo = Lo.find(params[:id])
     if lo.update!(lo_params)
-      render json: lo, status: :update
+      render json: lo, status: :ok
     else
-      render json: { lo: lo, errors: lo.errors}, status: :unprocessable_entity
+      render json: { lo: lo, errors: lo.errors }, status: :unprocessable_entity
     end
   end
 
-  # def destroy
-  #   lo = Lo.new(lo_params)
-  #   if lo.destroy(lo_params)
-  #     render json: lo, status: :destroy
-  #   else
-  #     render json: { lo: lo, errors: lo.errors}, status: :unprocessable_entity
-  #   end
-  # end
-
   def destroy
     Lo.find(params[:id]).destroy
-    head :no_content
+    render json: 'lo deletado com sucesso', status: :ok
   end
 
   private
