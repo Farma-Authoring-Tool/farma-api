f505c7dfc22473352c028eca6528fcaa774589a0
isabela-tvitek <isat12vitek@gmail.com>
2023-10-26 19:21:10 -0300

    feat(api): test corrections

--- test/controllers/api/professors/los/create_test.rb
+++ test/controllers/api/professors/los/create_test.rb
@@ -1,50 +1,52 @@
 require 'test_helper'
 
-class Api::Professors::ExercisesControllerCreateTest < ActionDispatch::IntegrationTest
-  setup do
-    @lo = FactoryBot.create(:lo)
-  end
-
+class Api::Professors::LosControllerCreateTest < ActionDispatch::IntegrationTest
   context 'create' do
     context 'with valid params' do
       should 'be successfully' do
-        exercise_attributes = FactoryBot.attributes_for(:exercise, lo: @lo)
-        post api_professors_lo_exercises_path(@lo), params: { exercise: exercise_attributes }, as: :json
+        lo_attributes = FactoryBot.attributes_for(:lo)
+
+        post api_professors_los_path, params: { lo: lo_attributes }, as: :json
 
         assert_response :created
         assert_equal RESPONSE::Type::JSON, response.content_type
         data = response.parsed_body
 
-        exercise = Exercise.last
-
-        assert_equal success_create_message(model: exercise), data['message']
-        assert_equal exercise_attributes[:description], data['exercise']['description']
-        assert_equal exercise_attributes[:public], data['exercise']['public']
-        assert_equal exercise_attributes[:position], data['exercise']['position']
-        assert_equal @lo.id, data['exercise']['lo_id']
-        assert_not_nil data['exercise']['id']
+        assert_equal success_create_message(model: Lo), data['message']
+        assert_equal lo_attributes[:title],       data['lo']['title']
+        assert_equal lo_attributes[:description], data['lo']['description']
+        assert_not_nil data['lo']['id']
       end
     end
 
     context 'with invalid params' do
       should 'be unsuccessfully' do
-        exercise_attributes = FactoryBot.attributes_for(
-          :exercise,
-          lo: @lo,
-          description: '',
-          public: '',
-          position: '',
-          lo_id: @lo
-        )
+        lo_attributes = FactoryBot.attributes_for(:lo, title: '', description: '')
 
-        post api_professors_lo_exercises_path(@lo), params: { exercise: exercise_attributes }, as: :json
+        post api_professors_los_path, params: { lo: lo_attributes }, as: :json
 
         assert_response :unprocessable_entity
         assert_equal RESPONSE::Type::JSON, response.content_type
         data = response.parsed_body
 
         assert_equal error_message, data['message']
+        assert_contains data['errors']['title'], I18n.t('errors.messages.blank')
         assert_contains data['errors']['description'], I18n.t('errors.messages.blank')
+      end
+
+      should 'be unsuccessfully when title already taken' do
+        lo = FactoryBot.create(:lo)
+
+        lo_attributes = FactoryBot.attributes_for(:lo, title: lo.title, description: '')
+
+        post api_professors_los_path, params: { lo: lo_attributes }, as: :json
+
+        assert_response :unprocessable_entity
+        assert_equal RESPONSE::Type::JSON, response.content_type
+        data = response.parsed_body
+
+        assert_equal error_message, data['message']
+        assert_contains data['errors']['title'], I18n.t('errors.messages.taken')
       end
     end
   end
