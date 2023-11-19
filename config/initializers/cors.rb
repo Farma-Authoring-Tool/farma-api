b59a9d400528d2a9558e69fe76f9054899e63717
isabela-tvitek <isat12vitek@gmail.com>
2023-11-18 21:27:30 -0300

    feat(api): adjusting secret key and tests

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:8000'
 
-     resource '*',
+    resource '*',
              expose: ['Authorization'],
              headers: :any,
              methods: [:get, :put, :post, :patch, :delete]
--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_15_203339) do
   create_table "introductions", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.boolean "public", default: true
+    t.boolean "public", default: true, null: false
     t.integer "position", default: 1
     t.bigint "lo_id"
     t.datetime "created_at", null: false
@@ -41,7 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_15_203339) do
   create_table "los", force: :cascade do |t|
     t.string "title"
     t.string "description"
-    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["title"], name: "index_los_on_title", unique: true
@@ -69,6 +68,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_15_203339) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["solution_step_id"], name: "index_tips_on_solution_step_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "jti"
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
