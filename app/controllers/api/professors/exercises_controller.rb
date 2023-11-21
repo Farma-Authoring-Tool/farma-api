d5c4d2399778030388f2345290ed07df70e55abb
isabela-tvitek <isat12vitek@gmail.com>
2023-11-21 02:02:38 -0300

    feat(api): created function to duplicate exercises

--- app/controllers/api/professors/exercises_controller.rb
+++ app/controllers/api/professors/exercises_controller.rb
@@ -37,8 +37,28 @@ class Api::Professors::ExercisesController < ApplicationController
   end
 
   def destroy
-    @exercise.destroy
-    render json: { message: success_destroy_message }, status: :accepted
+    @exercise = Exercise.find_by(id: params[:id], lo_id: params[:lo_id])
+
+    if @exercise
+      @exercise.destroy
+      render json: { message: success_destroy_message }, status: :accepted
+    else
+      render json: { message: 'Exercício não encontrado.' }, status: :not_found
+    end
+  end
+
+  def duplicate
+    duplicated_exercise = @exercise.duplicate
+
+    if duplicated_exercise.save
+      duplicated_exercise = Exercise.find(duplicated_exercise.id)
+      render json: { message: 'Exercício duplicado com sucesso', exercise: duplicated_exercise }, status: :created
+    else
+      render json: {
+        message: 'Erro ao duplicar o exercício',
+        errors: duplicated_exercise.errors
+      }, status: :unprocessable_entity
+    end
   end
 
   private
@@ -55,5 +75,8 @@ class Api::Professors::ExercisesController < ApplicationController
 
   def find_exercise
     @exercise = @lo.exercises.find(params[:id])
+    return unless @exercise.nil?
+
+    render json: { message: 'Exercício não encontrado.' }, status: :unprocessable_entity
   end
 end
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -8,7 +8,34 @@ class Exercise < ApplicationRecord
 
   before_create :set_position
 
+  def duplicate
+    duplicated_exercise = dup
+    duplicated_exercise.title = generate_duplicated_title
+
+    if duplicated_exercise.save
+      solution_steps.includes(:tips).find_each do |solution_step|
+        duplicated_step = solution_step.duplicate
+        duplicated_step.exercise = duplicated_exercise
+        duplicated_step.save
+      end
+    end
+
+    duplicated_exercise
+  end
+
   private
+
+  def generate_duplicated_title
+    copy_number = 1
+    new_title = "#{title} (cópia - #{copy_number})"
+
+    while self.class.exists?(title: new_title)
+      copy_number += 1
+      new_title = "#{title} (cópia - #{copy_number})"
+    end
+
+    new_title
+  end
 
   def set_position
     self.position = Time.now.to_i
