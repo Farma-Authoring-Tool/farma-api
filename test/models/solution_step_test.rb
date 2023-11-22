0d0000abf702a79adade42bd499d0a65a34f4b6c
isabela-tvitek <isat12vitek@gmail.com>
2023-11-21 21:24:03 -0300

    feat(api): adjusted solution_steps model test

--- test/models/solution_step_test.rb
+++ test/models/solution_step_test.rb
@@ -16,4 +16,30 @@ class SolutionStepTest < ActiveSupport::TestCase
     should have_many(:tips)
     should have_many(:tips).dependent(:destroy)
   end
+
+  context 'duplicating a solution step' do
+    setup do
+      @exercise = FactoryBot.create(:exercise)
+      @solution_step = FactoryBot.create(:solution_step, exercise: @exercise, title: 'Introduction to simple equations')
+      FactoryBot.create(:tip, solution_step: @solution_step, description: 'Learning about equations')
+    end
+
+    should 'create a new solution step with a modified title' do
+      duplicated_step = @solution_step.duplicate
+      duplicated_step.save!
+
+      assert_not_nil duplicated_step
+      assert_not_equal duplicated_step.id, @solution_step.id
+      assert_match(/Introduction to simple equations \(cópia - \d+\)/, duplicated_step.title)
+    end
+
+    should 'duplicate associated tip with a modified description' do
+      duplicated_step = @solution_step.duplicate
+      duplicated_step.save!
+      duplicated_tip = duplicated_step.tips.first
+
+      assert_equal 1, duplicated_step.tips.count
+      assert_match(/Learning about equations \(cópia - \d+\)/, duplicated_tip.description)
+    end
+  end
 end
