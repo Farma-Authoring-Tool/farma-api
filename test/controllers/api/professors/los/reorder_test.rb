2c4320004991cfa0a1094f30b90a5b20903e14a7
isabela-tvitek <isat12vitek@gmail.com>
2023-11-22 22:49:43 -0300

    feat(api): created controller reorder function test

--- test/controllers/api/professors/los/reorder_test.rb
+++ test/controllers/api/professors/los/reorder_test.rb
@@ -8,21 +8,31 @@ class Api::Professors::LosControllerReorderTest < ActionDispatch::IntegrationTes
     sign_in @user
     @lo = FactoryBot.create(:lo)
     @exercises = FactoryBot.create_list(:exercise, 3, lo: @lo)
+    @introductions = FactoryBot.create_list(:introduction, 2, lo: @lo)
   end
 
-  # test 'should correctly reorder exercises within a learning object' do
-  #   new_order_ids = @exercises.shuffle.map(&:id)
+  test 'should correctly reorder items within a learning object' do
+    new_order = [
+      { id: @introductions[0].id, type: 'Introduction', position: 1 },
+      { id: @exercises[1].id, type: 'Exercise', position: 2 },
+      { id: @exercises[0].id, type: 'Exercise', position: 3 },
+      { id: @introductions[1].id, type: 'Introduction', position: 4 },
+      { id: @exercises[2].id, type: 'Exercise', position: 5 }
+    ]
 
-  #   post reorder_items_api_professors_lo_path(@lo),
-  #     params: { items: new_order_ids.map { |id| { id: id, type: 'Exercise', position: new_order_ids.index(id) + 1 } } },
-  #     as: :json
+    post reorder_items_api_professors_lo_path(@lo),
+      params: { items: new_order },
+      as: :json
     
-  #   @lo.reload
-    
-  #   assert_response :ok
-  #   new_order_ids.each_with_index do |id, index|
-  #     exercise = @exercises.find(id)
-  #     assert_equal index + 1, exercise.position
-  #   end 
-  # end
+    @lo.reload
+
+    ordered_exercises = @lo.exercises.order(:position)
+    ordered_introductions = @lo.introductions.order(:position)
+
+    assert_equal new_order[0][:id], ordered_introductions[0].id
+    assert_equal new_order[1][:id], ordered_exercises[1].id
+    assert_equal new_order[2][:id], ordered_exercises[0].id
+    assert_equal new_order[3][:id], ordered_introductions[1].id
+    assert_equal new_order[4][:id], ordered_exercises[2].id
+  end
 end
