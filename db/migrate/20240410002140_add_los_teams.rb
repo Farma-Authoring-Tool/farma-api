9ac9bf7f50e54ef773cf0f4d35ccb7ca5044c37f
Douglas Vinicius <vinibonin007@gmail.com>
2024-04-11 21:54:19 -0300

    chore(migrations): add tables for teams

---
+++ db/migrate/20240410002140_add_los_teams.rb
@@ -0,0 +1,8 @@
+class AddLosTeams < ActiveRecord::Migration[7.1]
+  def change
+    create_table :los_teams, id: false do |t|
+      t.references :team, foreign_key: true
+      t.references :lo, foreign_key: true
+    end
+  end
+end
