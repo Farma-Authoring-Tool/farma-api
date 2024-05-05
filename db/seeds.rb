73fd6295be8aa74c7e02f4389ec56f377d85f950
Douglas Vinicius <vinibonin007@gmail.com>
2024-05-05 19:08:26 -0300

    Corrige mapeamento de descrição.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+professor = User.create(email: 'testeprofessor@teste.com', password: '123456', password_confirmation: '123456',
+                        name: 'teste professor')
+student = User.create(email: 'testealuno@teste.com', password: '123456', password_confirmation: '123456',
+                      name: 'teste aluno')
+
+lo = Lo.create(
+  title: 'Objeto de aprendizagem teste',
+  description: 'descrição teste',
+  user_id: professor.id
+)
+
+Introduction.create(
+  title: 'intro 1',
+  description: 'conteudo introducao 1',
+  public: true,
+  lo_id: lo.id
+)
+
+exercise = Exercise.create(
+  title: 'exercicio 1',
+  description: 'teste exercicio 1',
+  public: true,
+  lo_id: lo.id
+)
+
+SolutionStep.create(
+  title: 'passo de solucao 1',
+  description: 'descricao passo solucao 1',
+  response: 'valor de x',
+  decimal_digits: 2,
+  public: true,
+  exercise_id: exercise.id
+)
+
+team = Team.create(name: 'Turma 1', code: 'turma1', user_id: professor.id)
+team.users << student
+team.los << lo
